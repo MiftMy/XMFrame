addd5554d7763b4222617a3be126c5e53cf1a7cc
Mifit <170085130@qq.com>
2019-01-21 09:46:14 +0800

    Initial Commit

---
+++ XMFrame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XMFrame
+//
+//  Created by Mifit on 2019/1/21.
+//  Copyright © 2019年 Mifit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XMFrame/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XMFrame
+//
+//  Created by Mifit on 2019/1/21.
+//  Copyright © 2019年 Mifit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
